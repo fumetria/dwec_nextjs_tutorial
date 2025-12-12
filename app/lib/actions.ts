44e7105c3d3ade2f9045ae67181cdf8fa231f02a
Fu Jun <fulin.fujun@outlook.es>
2025-12-13 00:57:01 +0100

    Chapter 11 - Create Invoice - done

---
+++ app/lib/actions.ts
@@ -0,0 +1,36 @@
+'use server';
+
+import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+//zod --> z.coerce is a powerful feature that automatically converts input data (like strings from URLs or forms) into a target data type (number, boolean, etc.)
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+export async function createInvoice(formData: FormData) {
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
