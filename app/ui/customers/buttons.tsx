2924aeb22f9730fa853109b227d13a5bafdd396a
Fu Jun <fulin.fujun@outlook.es>
2025-12-24 00:42:07 +0100

    Add ShowCustomerInvoices button in customers page to show invoices from one customer

--- app/ui/customers/buttons.tsx
+++ app/ui/customers/buttons.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link";
-import { PlusIcon, PencilIcon, TrashIcon } from "@heroicons/react/24/outline";
+import {
+  PlusIcon,
+  PencilIcon,
+  TrashIcon,
+  DocumentMagnifyingGlassIcon,
+} from "@heroicons/react/24/outline";
 
 export function CreateCustomer() {
   return (
     <Link
       href="/dashboard/customers/create"
-      className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+      className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 dark:bg-cyan-500 dark:text-slate-50 dark:hover:bg-cyan-400 dark:focus-visible:outline-cyan-600"
     >
       <span className="hidden md:block">New Customer</span>{" "}
       <PlusIcon className="h-5 md:ml-4" />
@@ -33,5 +38,17 @@ export function DeleteCustomer({ id }: { id: string }) {
         </button>
       </form>
     </>
+  );
+}
+
+export function ShowCustomerInvoices({ name }: { name: string }) {
+  return (
+    <Link
+      href={`/dashboard/invoices?query=${name}`}
+      className="rounded p-2 hover:bg-gray-100 border border-gray-50 shadow hover:border-gray-100"
+      title={`Show invoices from ${name}`}
+    >
+      <DocumentMagnifyingGlassIcon className="w-5" />
+    </Link>
   );
 }
